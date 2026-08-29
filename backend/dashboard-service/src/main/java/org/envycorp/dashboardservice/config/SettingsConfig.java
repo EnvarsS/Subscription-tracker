451c13aacbb7112ef9a67774bae84667449b5483
Vladyslav <eddiedipper4228@gmail.com>
2026-08-29 13:56:31 +0200

    Set up modelmapper in config class

---
+++ backend/dashboard-service/src/main/java/org/envycorp/dashboardservice/config/SettingsConfig.java
@@ -0,0 +1,13 @@
+package org.envycorp.dashboardservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SettingsConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
