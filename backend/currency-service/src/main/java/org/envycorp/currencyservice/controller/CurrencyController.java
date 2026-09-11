01749402d0067ad84c3630301b0d1a4de6f58017
Vladyslav <eddiedipper4228@gmail.com>
2026-09-11 17:52:12 +0200

    Added missing dependencies and initialized service controller

---
+++ backend/currency-service/src/main/java/org/envycorp/currencyservice/controller/CurrencyController.java
@@ -0,0 +1,12 @@
+package org.envycorp.currencyservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api/currencies")
+public class CurrencyController {
+
+}
